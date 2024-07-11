ee0c791f25ab3323301a22531c66e705857dfe59
Murali Prasanth <mprasanth18@gmail.com>
2024-07-11 23:35:58 +0530

    added fibonacci with iterative solution

--- js/Fibonacci/fibonacci.js
+++ js/Fibonacci/fibonacci.js
@@ -10,5 +10,17 @@ function fib(n, memo = {}) {
 	memo[n] = result;
 	return result;
 }
+
+function fibIterative(n) {
+	const result = new Array(n + 1);
+	result[0] = 0;
+	result[1] = 1;
+	for (let i = 2; i <= n; i++) {
+		result[i] = result[i - 1] + result[i - 2];
+	}
+
+	return result[n];
+}
 console.clear();
 console.log(fib(100));
+console.log(fibIterative(100));
